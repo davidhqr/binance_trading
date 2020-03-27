a95d64654edda5b35440ff5fe8c7e426c29efeb9
David Zhang <davidhqr@gmail.com>
2020-03-26 18:46:34 -0700

    Add logging

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python3
+
 import datetime
+import logging
 
 import pandas as pd
 import pandas_ta as ta
@@ -16,6 +19,9 @@ df = None
 trade_executed = False
 trade_enter_price = 0
 trade_amount = 0
+
+logging.basicConfig(filename='binance_trading.log', level=logging.DEBUG, format='[%(asctime)s] %(message)s',
+                    datefmt='%d/%m/%Y %H:%M:%S')
 
 
 def get_asset_balance(asset):
@@ -70,7 +76,7 @@ def process_message(msg):
         ac = df['AC'].iat[-1]
         ac_change = ac - df['AC'].iat[-2]
         open_time_formatted = datetime.datetime.fromtimestamp(int(open_time) / 1000)
-        print('[%s] %s | Close: %0.8f | +DI: %0.8f | AC: %0.8f' % (open_time_formatted, TICKER, close, plus, ac))
+        logging.info('[%s] %s | Close: %0.8f | +DI: %0.8f | AC: %0.8f' % (open_time_formatted, TICKER, close, plus, ac))
 
         buy = ((plus < 10 or prev_plus < 10) and ac < 0 and ac_change > 0) and not trade_executed
         sell = (((plus > 20 or prev_plus > 20) and ac > 0 and ac_change <= 0) or (
@@ -78,25 +84,25 @@ def process_message(msg):
 
         if buy:
             trade_amount = round((get_asset_balance(MAIN_SYMBOL) * 0.98) / close, 2)
-            print('[Alert] Buy %s of %s at price %0.8f' % (trade_amount, TICKER, close))
+            logging.info('[Alert] Buy %s of %s at price %0.8f' % (trade_amount, TICKER, close))
             order = client.create_margin_order(
                 symbol=TICKER,
                 side=SIDE_BUY,
                 type=ORDER_TYPE_MARKET,
-                quantity=50
+                quantity=trade_amount
             )
             if order['status'] == "FILLED":
-                print('[Order] Bought %s of %s at %0.8f' % (trade_amount, TICKER, close))
+                logging.info('[Order] Bought %s of %s at %0.8f' % (trade_amount, TICKER, close))
                 trade_executed = True
                 trade_enter_price = close
             else:
-                print('[ERROR] Order to buy %s of %s at %0.8f was not filled' % (trade_amount, TICKER, close))
+                logging.info('[ERROR] Order to buy %s of %s at %0.8f was not filled' % (trade_amount, TICKER, close))
 
         if sell:
             profit_pct = (close - trade_enter_price) / trade_enter_price * 100
             sell_amount = get_asset_balance(FOREIGN_SYMBOL) // 0.01 * 0.01  # round down to 2 decimals
             formatted_sell_amount = "{:0.0{}f}".format(sell_amount, 2)
-            print('[Alert] Sell %s of %s at price %0.8f. Profit: %0.2f%%' % (
+            logging.info('[Alert] Sell %s of %s at price %0.8f. Profit: %0.2f%%' % (
                 formatted_sell_amount, TICKER, close, profit_pct))
             order = client.create_margin_order(
                 symbol=TICKER,
@@ -105,12 +111,12 @@ def process_message(msg):
                 quantity=formatted_sell_amount
             )
             if order['status'] == "FILLED":
-                print('[Order] Sold %s of %s at %0.8f' % (sell_amount, TICKER, close))
+                logging.info('[Order] Sold %s of %s at %0.8f' % (sell_amount, TICKER, close))
                 trade_executed = False
                 trade_enter_price = 0
                 trade_amount = 0
             else:
-                print('[ERROR] Order to sell %s of %s at %0.8f was not filled' % (trade_amount, TICKER, close))
+                logging.info('[ERROR] Order to sell %s of %s at %0.8f was not filled' % (trade_amount, TICKER, close))
 
 
 # Add heiken ashi candles to dataframe
@@ -128,6 +134,7 @@ def add_heiken_ashi():
 
 
 # Run program
+logging.info("Starting binance trading bot")
 client = Client(public_key, secret_key)
 
 # Load historical candles into dataframe
