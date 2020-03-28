e00b16d8ec79579b5cdd0e11078ebbfad88c5b4f
David Zhang <davidhqr@gmail.com>
2020-03-27 20:05:44 -0700

    Add logging

--- main.py
+++ main.py
@@ -46,15 +46,15 @@ def process_message(msg):
     global df, trade_executed, trade_enter_price, trade_amount
     candle = msg['k']
     is_final = candle['x']
+    close_time = datetime.datetime.fromtimestamp(int(candle['T']) / 1000)
 
-    if is_final and df['close_time'].iat[-1] != candle['T']:
+    if is_final and df['close_time'].iat[-1] != close_time:
         # Append candle to dataframe
-        open_time = candle['t']
+        open_time = datetime.datetime.fromtimestamp(int(candle['t']) / 1000)
         open_ = float(candle['o'])
         high = float(candle['h'])
         low = float(candle['l'])
         close = float(candle['c'])
-        close_time = candle['T']
         ha_close = (open_ + high + low + close) / 4
         ha_open = (df['ha_open'].iat[-1] + df['ha_close'].iat[-1]) / 2
         ha_high = max(ha_open, ha_close, high)
@@ -83,9 +83,8 @@ def process_message(msg):
         prev_plus = df['DMP_14'].iat[-2]
         ac = df['AC'].iat[-1]
         ac_change = ac - df['AC'].iat[-2]
-        open_time_formatted = datetime.datetime.fromtimestamp(int(open_time) / 1000)
         logging.info(
-            '[%s] %s | Close: %0.8f | +DI: %0.8f | AC: %0.8f', open_time_formatted, TICKER, ha_close, plus, ac)
+            '[%s] %s | Close: %0.8f | +DI: %0.8f | AC: %0.8f', close_time, TICKER, ha_close, plus, ac)
 
         buy = ((plus < 11.8 or prev_plus < 11.8) and ac < 0 and ac_change > 0) and not trade_executed
         sell = (((plus > 20.2 or prev_plus > 20.2) and ac > 0 and ac_change <= 0) or (
@@ -94,18 +93,18 @@ def process_message(msg):
         if buy:
             trade_amount = round((get_asset_balance(MAIN_SYMBOL) * 0.98) / ha_close, 2)
             logging.info('[Alert] Buy %s of %s at price %0.8f', trade_amount, TICKER, ha_close)
-            # order = client.create_margin_order(
-            #     symbol=TICKER,
-            #     side=SIDE_BUY,
-            #     type=ORDER_TYPE_MARKET,
-            #     quantity=trade_amount
-            # )
-            # if order['status'] == "FILLED":
-            #     logging.info('[Order] Bought %s of %s at %0.8f', trade_amount, TICKER, ha_close)
-            #     trade_executed = True
-            #     trade_enter_price = ha_close
-            # else:
-            #     logging.info('[ERROR] Order to buy %s of %s at %0.8f was not filled', trade_amount, TICKER, ha_close)
+            order = client.create_margin_order(
+                symbol=TICKER,
+                side=SIDE_BUY,
+                type=ORDER_TYPE_MARKET,
+                quantity=trade_amount
+            )
+            if order['status'] == "FILLED":
+                logging.info('[Order] Bought %s of %s at %0.8f', trade_amount, TICKER, ha_close)
+                trade_executed = True
+                trade_enter_price = ha_close
+            else:
+                logging.info('[ERROR] Order to buy %s of %s at %0.8f was not filled', trade_amount, TICKER, ha_close)
 
         if sell:
             profit_pct = (ha_close - trade_enter_price) / trade_enter_price * 100
@@ -113,19 +112,19 @@ def process_message(msg):
             formatted_sell_amount = "{:0.0{}f}".format(sell_amount, 2)
             logging.info('[Alert] Sell %s of %s at price %0.8f. Profit: %0.2f%%', formatted_sell_amount, TICKER,
                          ha_close, profit_pct)
-            # order = client.create_margin_order(
-            #     symbol=TICKER,
-            #     side=SIDE_SELL,
-            #     type=ORDER_TYPE_MARKET,
-            #     quantity=formatted_sell_amount
-            # )
-            # if order['status'] == "FILLED":
-            #     logging.info('[Order] Sold %s of %s at %0.8f', sell_amount, TICKER, ha_close)
-            #     trade_executed = False
-            #     trade_enter_price = 0
-            #     trade_amount = 0
-            # else:
-            #     logging.info('[ERROR] Order to sell %s of %s at %0.8f was not filled', trade_amount, TICKER, ha_close)
+            order = client.create_margin_order(
+                symbol=TICKER,
+                side=SIDE_SELL,
+                type=ORDER_TYPE_MARKET,
+                quantity=formatted_sell_amount
+            )
+            if order['status'] == "FILLED":
+                logging.info('[Order] Sold %s of %s at %0.8f', sell_amount, TICKER, ha_close)
+                trade_executed = False
+                trade_enter_price = 0
+                trade_amount = 0
+            else:
+                logging.info('[ERROR] Order to sell %s of %s at %0.8f was not filled', trade_amount, TICKER, ha_close)
 
 
 # Add heiken ashi candles to dataframe
@@ -148,15 +147,24 @@ client = Client(public_key, secret_key)
 
 # Load historical candles into dataframe
 historical_klines = client.get_historical_klines(TICKER, Client.KLINE_INTERVAL_5MINUTE, '1 day ago UTC')
-historical_candles = map(lambda kline: kline[:7], historical_klines)
+historical_candles = list(map(lambda kline: kline[:7], historical_klines))
 column_names = ['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time']
 df = pd.DataFrame(data=historical_candles, columns=column_names)
+df['open_time'] = df['open_time'].apply(lambda x: datetime.datetime.fromtimestamp(int(x) / 1000))
 df['open'] = df['open'].astype(float)
 df['high'] = df['high'].astype(float)
 df['low'] = df['low'].astype(float)
 df['close'] = df['close'].astype(float)
 df['volume'] = df['volume'].astype(float)
+df['close_time'] = df['close_time'].apply(lambda x: datetime.datetime.fromtimestamp(int(x) / 1000))
 add_heiken_ashi()
+df.ta.adx(high=df['ha_high'], low=df['ha_low'], close=df['ha_close'], length=14, append=True)
+df.ta.ao(high=df['ha_high'], low=df['ha_low'], append=True)
+df.ta.sma(close=df['AO_5_34'], length=5, append=True)
+df['AC'] = df['AO_5_34'] - df['SMA_5']
+
+logging.info('[%s] %s | Close: %0.8f | +DI: %0.8f | AC: %0.8f', df['close_time'].iat[-1], TICKER,
+             df['ha_close'].iat[-1], df['DMP_14'].iat[-1], df['AC'].iat[-1])
 
 # Start listening for live candles
 bm = BinanceSocketManager(client, user_timeout=60)
